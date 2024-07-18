4626f8c80855998bdc3823e17829f9a5c455c9b1
veerabv <bveera0608@gmail.com>
2024-07-18 18:51:29 +0530

    add asyncawait logic

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,19 +1,17 @@
 import Places from "./Places.jsx";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availabelPlaces , setAvailablePlaces] = useState([]);
-  useEffect(
-    () =>{
-      fetch("http://localhost:3000/places")
-        .then((response) => {
-          return response.json();
-        })
-        .then((resData) => {
-          setAvailablePlaces(resData.places);
-        })},
-    []
-  );
+  const [availabelPlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetching() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetching();
+  }, []);
   return (
     <Places
       title="Available Places"
