3935b76a4f8bb4b3d95708d615cc4d5268932064
veerabv <bveera0608@gmail.com>
2024-07-18 19:14:47 +0530

    handling the ui till the http req send response

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching , setIsFetching] = useState(false);
   const [availabelPlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetching() {
+      setIsFetching(true)
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false)
     }
     fetching();
   }, []);
@@ -16,6 +19,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availabelPlaces}
+      isLoading = {isFetching}
+      loadingText = "fetching places ...."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
